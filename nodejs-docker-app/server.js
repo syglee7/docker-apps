36c107cd1448206db1136e9b523b9595b7126ab6
zenaislee <syglee7@gmail.com>
2021-01-07 23:40:00 +0900

    docker volume and not install node_modules again

--- nodejs-docker-app/server.js
+++ nodejs-docker-app/server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 
-const PORT = 5000;
+const PORT = 5050;
 
 
 // APP
 const app = express();
 app.get('/', (req, res) => {
-    res.send("Hello, World");
+    res.send("Hello, World~!~!");
 });
 
 app.listen(PORT);

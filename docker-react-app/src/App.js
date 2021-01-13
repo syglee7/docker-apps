42e6c38167e8ae78d41cc22a9d5ca1d3afacacd6
zenaislee <syglee7@gmail.com>
2021-01-13 23:58:30 +0900

    test volume

--- docker-react-app/src/App.js
+++ docker-react-app/src/App.js
@@ -15,8 +15,14 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          리액트 배우기!!
+          learn react
           {/* docker run -it -p 3000:3000 -v /usr/src/app/node_modules -v $(pwd):/usr/src/app syglee7/docker-react-app */}
+
+          {
+            /* docker 환경에서 테스트 명려어
+            docker run -it syglee7/docker-react-app npm run test
+             */
+          }
         </a>
       </header>
     </div>
